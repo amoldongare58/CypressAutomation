d0995f54989e18d4e9ca723c57d56fa0842d8bed
amoldongare58 <amoldongare58@gmail.com>
2024-12-01 12:59:11 +0530

    Pushed the 2 nd changes

--- cypress/e2e/myFirsttest.cy.js
+++ cypress/e2e/myFirsttest.cy.js
@@ -11,6 +11,7 @@ describe('my first test ', function()
     {
       cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
       cy.title().should('eq','Orangehrm')
+      
     }
       )
 }
