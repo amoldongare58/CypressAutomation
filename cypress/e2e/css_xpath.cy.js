c9fd25e1aa74adb86ce9d6018956719cc2486cd3
amoldongare58 <amoldongare58@gmail.com>
2024-12-01 12:49:00 +0530

    Pushing frmework to branch

---
+++ cypress/e2e/css_xpath.cy.js
@@ -0,0 +1,17 @@
+
+describe ('suit name', function ()
+{
+    it ('css selector',function ()
+    {
+        cy.visit("https://automationexercise.com/products")
+
+       cy.get("input#search_product").type("tshirts")
+
+       cy.get("button#submit_search").click
+       
+    }
+)
+
+
+}
+)
---
+++ cypress/e2e/myFirsttest.cy.js
@@ -0,0 +1,17 @@
+describe('my first test ', function() 
+{
+    it('verify title positive', function() 
+    {
+      cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+      cy.title().should('eq','OrangeHRM')
+    }
+      )
+
+      it('verify title negative', function() 
+    {
+      cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+      cy.title().should('eq','Orangehrm')
+    }
+      )
+}
+)
